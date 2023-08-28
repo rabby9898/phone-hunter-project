3ddd3501b2db178fe5cf2aed993a8e4d74850785
rabby9898 <mdfajlerabby00@gmail.com>
2023-08-28 10:50:31 +0600

    js file added and Data load by fetching from server

--- js/hunter.js
+++ js/hunter.js
@@ -1,1 +1,9 @@
-console.log("ready")
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phoneData = data.data;
+  console.log(phoneData);
+};
+loadPhone();
